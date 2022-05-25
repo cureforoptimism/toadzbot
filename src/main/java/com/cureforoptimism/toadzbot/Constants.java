70dd0727acff24bfe1d38dc4e1675bca2d670934
Cure For Optimism <cure_for_optimism@protonmail.com>
2022-05-24 22:39:41 -0600

    Add sales channel ID

--- src/main/java/com/cureforoptimism/toadzbot/Constants.java
+++ src/main/java/com/cureforoptimism/toadzbot/Constants.java
@@ -1,7 +1,7 @@
 package com.cureforoptimism.toadzbot;
 
 public class Constants {
-  public static final long CHANNEL_SALES_BOT = 926303694302109796L;
+  public static final long CHANNEL_SALES_BOT = 978879710329184286L;
   public static final long CHANNEL_TEST_GENERAL = 926303694801223751L;
   public static final String TOADSTOOLZ_CONTRACT_ID = "0x09CAE384C6626102ABE47Ff50588A1dBe8432174";
 }
